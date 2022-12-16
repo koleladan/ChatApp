1296b57c276594deea9137a3f76d9168cb1aa6f7
koleladan <isaiahodan@gmail.com>
2022-12-16 22:50:57 +0300

    intents

--- app/src/main/java/com/example/chatapp/LoginActivity.kt
+++ app/src/main/java/com/example/chatapp/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.login.setOnClickListener{
+            val intent= Intent(this@LoginActivity,MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.signup.setOnClickListener {
+            val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/chatapp/SignUpActivity.kt
+++ app/src/main/java/com/example/chatapp/SignUpActivity.kt
@@ -1,11 +1,19 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatapp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.signup.setOnClickListener {
+            val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
