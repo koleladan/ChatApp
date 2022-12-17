d9e882a7a0d2c73c13057465c2ced511e80b1935
koleladan <isaiahodan@gmail.com>
2022-12-17 22:53:54 +0300

    login and signup logic using firebase authentication system

--- app/src/main/java/com/example/chatapp/LoginActivity.kt
+++ app/src/main/java/com/example/chatapp/LoginActivity.kt
@@ -1,24 +1,63 @@
 package com.example.chatapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.chatapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mAuth = FirebaseAuth.getInstance()
+
+
         binding.login.setOnClickListener{
-            val intent= Intent(this@LoginActivity,MainActivity::class.java)
-            startActivity(intent)
+            val email = binding.email.text.toString()
+            val password = binding.edtpassword.text.toString()
+
+            if(email.isEmpty()) {
+                binding.email.error = "Enter email"
+            }
+            else if (password.isEmpty()) {
+                binding.edtpassword.error = "Enter password"
+            }
+            else{
+                login(email, password)
+            }
+
         }
         binding.signup.setOnClickListener {
-            val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this@LoginActivity,SignUpActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
         }
+    }
+
+    private fun login(email: String, password: String) {
+
+        val progressDialog = ProgressDialog(this@LoginActivity)
+        progressDialog.setMessage("Loading...")
+        progressDialog.setCancelable(false)
+        progressDialog.show()
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){task ->
+                if (task.isSuccessful){
+                    progressDialog.dismiss()
+                    val intent= Intent(this@LoginActivity,MainActivity::class.java)
+                    startActivity(intent)
+
+                }else{
+                    progressDialog.dismiss()
+                    Toast.makeText(applicationContext, "User does not exist",Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 }
--- app/src/main/java/com/example/chatapp/SignUpActivity.kt
+++ app/src/main/java/com/example/chatapp/SignUpActivity.kt
@@ -1,19 +1,63 @@
 package com.example.chatapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.chatapp.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        mAuth = FirebaseAuth.getInstance()
         binding.signup.setOnClickListener {
-            val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
-            startActivity(intent)
+            signup()
         }
+    }
+
+    private fun signup() {
+        val  fname = binding.edtfname.text.toString()
+        val lname = binding.edtlname.text.toString()
+        val email = binding.email.text.toString()
+        val password = binding.edtpassword.text.toString()
+
+        if(fname.isEmpty()){
+            binding.edtfname.error = "Enter first name"
+        }
+        else if (lname.isEmpty()){
+            binding.edtlname.error = "Enter last name"
+        }
+        if(email.isEmpty()){
+            binding.email.error = "Enter username"
+        }
+        else if(password.isEmpty()){
+            binding.edtpassword.error = "Enter password"
+        }
+        val progressDialog =ProgressDialog(this@SignUpActivity)
+        progressDialog.setMessage("Loading...")
+        progressDialog.setCancelable(false)
+        progressDialog.show()
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){ task ->
+                if (task.isSuccessful){
+                    progressDialog.dismiss()
+                    val intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                    startActivity(intent)
+
+                }else{
+                    progressDialog.dismiss()
+                    Toast.makeText(applicationContext, "some error occurred", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
     }
 }
